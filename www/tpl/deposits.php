51e5a9f66b3de05043764e5edcf41a47ff2d8aa7
Dmitry Sizon <dimonsizon@mail.ru>
2016-01-31 14:27:50 +0300

    Добавил общий хедер таблицы и переделал взносы php

--- www/tpl/deposits.php
+++ www/tpl/deposits.php
@@ -2,10 +2,25 @@
 defined('ACCESS') or die();
 $s = 0;
 $result	= mysql_query("SELECT * FROM `deposits` WHERE `user_id` = ".$user_id." ORDER BY id ASC");
-while($row = mysql_fetch_array($result)) {
 
-	$result2	= mysql_query("SELECT * FROM `plans` WHERE `id` = ".$row['plan']." LIMIT 1");
-	$row2		= mysql_fetch_array($result2);
+print "<table class=\"contribution-table\">
+	<thead>
+		<tr>
+			<th>Дата</th>
+			<th>Сумма</th>
+			<th>Валюта</th>
+			<th>%</th>
+			<th>Осталось дней</th>
+			<th>Начислений в день</th>
+			<th>Начислено</th>
+		</tr>
+	</thead>
+	<tbody>";
+
+	while($row = mysql_fetch_array($result)) { // выводим взносы
+
+		$result2	= mysql_query("SELECT * FROM `plans` WHERE `id` = ".$row['plan']." LIMIT 1");
+		$row2		= mysql_fetch_array($result2);
 	
 	
 
@@ -49,44 +64,29 @@ print "<tr>
 print "</table>";
 */
 
-print "<table class=\"contribution-table\">
-	<thead>
-		<tr>
-			<th>Дата</th>
-			<th>Сумма</th>
-			<th>Валюта</th>
-			<th>%</th>
-			<th>Осталось дней</th>
-			<th>Начислений в день</th>
-			<th>Начислено</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>".date("d.m.Y H:i", $row['date'])."</td>
-			<td>".$row['sum']." ".$moneycurr;
-				if($row2['back'] == 1 && $row2['close'] == 1) {
-					print " [ <a href=\"javascript: if(confirm('".$lang['msg_09']." ".$row2['close_percent']."%')) top.location.href='/deposits/?close=".$row['id']."';\">".$lang['clousedepo']."</a> ]";
-				}			
-			print "</td>			
-			<td>".$moneycurr."</td>
-			<td>1% "; 
-				if($row2['period'] == 1) { print $land['inday']; } 
-					elseif($row2['period'] == 2) { print $land['week']; }  
-						elseif($row2['period'] == 4) { print $land['inhour']; } 
-							else { print $land['inmon']; }			
-			print "</td>
-			<td>Всего ".$row2['days']." ";
-				if($row2['period'] == 4) { print $land['hours']; } 
-					elseif($row2['period'] == 1) { print $land['deys']; } 
-						elseif($row2['period'] == 2) { print $land['weekly']; } 
-							elseif($row2['period'] == 3) { print $land['mesyac']; }
-			print "</td>
-			<td>Начислений в день</td>
-			<td>Начислено</td>
-		</tr>
-	</tbody>
-</table>";
+	print "<tr>
+				<td>".date("d.m.Y H:i", $row['date'])."</td>
+				<td>".$row['sum']." ".$moneycurr;
+					if($row2['back'] == 1 && $row2['close'] == 1) {
+						print " [ <a href=\"javascript: if(confirm('".$lang['msg_09']." ".$row2['close_percent']."%')) top.location.href='/deposits/?close=".$row['id']."';\">".$lang['clousedepo']."</a> ]";
+					}			
+				print "</td>			
+				<td>".$moneycurr."</td>
+				<td>1% "; 
+					if($row2['period'] == 1) { print $land['inday']; } 
+						elseif($row2['period'] == 2) { print $land['week']; }  
+							elseif($row2['period'] == 4) { print $land['inhour']; } 
+								else { print $land['inmon']; }			
+				print "</td>
+				<td>Всего ".$row2['days']." ";
+					if($row2['period'] == 4) { print $land['hours']; } 
+						elseif($row2['period'] == 1) { print $land['deys']; } 
+							elseif($row2['period'] == 2) { print $land['weekly']; } 
+								elseif($row2['period'] == 3) { print $land['mesyac']; }
+				print "</td>
+				<td>Начислений в день</td>
+				<td>Начислено</td>
+			</tr>";
 
 /*Ниже прегресс бар сколько осталось до следующей выплаты
 if(cfgSET('autopercent') == "on") {
@@ -108,8 +108,11 @@ print "<tr>
 </tr>";
 }*/
 
-$s = $s + $row['sum'];
-}
+	$s = $s + $row['sum'];
+	}
+
+print "</tbody>
+</table>";
 
 if($s == 0) {
 	print '<p class="er">'.$lang['msg_11'].'</p>';
