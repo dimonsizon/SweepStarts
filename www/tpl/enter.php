9ee69794c5ae624319c93f73f5b37e8c218788ce
Dmitry Sizon <dimonsizon@mail.ru>
2016-02-05 02:02:03 +0300

    Переделал страницу пополнения балланса

--- www/tpl/enter.php
+++ www/tpl/enter.php
@@ -1,30 +1,37 @@
 <?php
 defined('ACCESS') or die();
 ?>
-	<table align="center">
+<div class="enter-balance">
 	<form action="/enter/?action=save" method="post">
-	<tr><td><b><?php print $lang['sum']; ?></b>: </td><td align="right"><input style="width: 200px;" type='text' name='sum' value='' size="30" maxlength="7" /></td></tr>
-	<tr><td><b><?php print $lang['paysystem']; ?></b>: </td><td align="right">
-	<select style="width: 200px; margin-right: 0px;" name="ps">
-		<?php
-			if($cfgPerfect) { 
-				print '<option value="1">PerfectMoney</option>';
-			} 
-			if(cfgSET('cfgPEsid') && cfgSET('cfgPEkey')) { 
-				print '<option value="2">PAYEER.com</option>';
-			} 
+		<div class="form-container">
+			<div class="form-field">
+				<label><?php print $lang['sum']; ?>:</label>
+				<input class="text-input" type='text' name='sum' value='' size="30" maxlength="7" />
+			</div>
+			<div class="form-field">
+				<label><?php print $lang['paysystem']; ?>:</label>
+				<select name="ps">
+					<?php
+						if($cfgPerfect) { 
+							print '<option value="1">PerfectMoney</option>';
+						} 
+						if(cfgSET('cfgPEsid') && cfgSET('cfgPEkey')) { 
+							print '<option value="2">PAYEER.com</option>';
+						} 
 
-			$result	= mysql_query("SELECT * FROM `paysystems` WHERE id > 2 ORDER BY id ASC");
-			while($row = mysql_fetch_array($result)) {
-				print '<option value="'.$row['id'].'">'.$row['name'].'</option> ';
-			}
-		?>
-	</select></td></tr>
-	<tr><td></td><td align="right"><input style="width: 200px;" class="subm" type='submit' name='submit' value=' <?php print $lang['enterbalance']; ?> ' /></td></tr>
+						$result	= mysql_query("SELECT * FROM `paysystems` WHERE id > 2 ORDER BY id ASC");
+						while($row = mysql_fetch_array($result)) {
+							print '<option value="'.$row['id'].'">'.$row['name'].'</option> ';
+						}
+					?>
+				</select>
+			</div>
+			<div class="form-buttons">
+				<input class="subm" type='submit' name='submit' value=' <?php print $lang['enterbalance']; ?> ' />
+			</div>
+		</div>
 	</form>
-	</table><hr />
-
-<h3><?php print $lang['history']; ?>:</h3>
+	<h3><?php print $lang['history']; ?>:</h3>
 <?php
 	$num	= 20;
 	$p		= intval($_GET['p']);
@@ -42,7 +49,15 @@ defined('ACCESS') or die();
 		print "<p class=\"er\">".$lang['nodata']."</p>";
 	} else {
 
-		print "<table width=\"100%\" class=\"table-content\"><tr align=\"center\"><th><b>#</b></th><th width=\"100\"><b>".$lang['date']."</b></th><th><b>".$lang['sum']."</b></th><th><b>".$lang['schet']."</b></th><th><b>".$lang['system']."</b></th><th><b>".$lang['status']."</b></th></tr>";
+		print "<table class=\"table-content\">
+		<tr>
+			<th>#</th>
+			<th>".$lang['date']."</th>
+			<th>".$lang['sum']."</th>
+			<th>".$lang['schet']."</th>
+			<th>".$lang['system']."</th>
+			<th style=\"text-align: center\">".$lang['status']."</th>
+		</tr>";
 
 		$i = 1;
 		$s = 0;
@@ -50,25 +65,25 @@ defined('ACCESS') or die();
 
 		if($i % 2) { $bg = ""; } else { $bg = " bgcolor=\"#eeeeee\""; }
 
-		print "<tr".$bg." align=\"center\">
-		<td style=\"padding: 3px;\">".$row['id']."</td>
-		<td>".date("d.m.Y H:i", $row['date'])."</td>
-		<td>".$row['sum']." ".$moneycurr."</td>
-		<td><b>".$row['purse']."</b></td>
-		<td>".$row['paysys']."</td>
-		<td class=\"status\">";
+		print "<tr".$bg.">
+			<td>".$row['id']."</td>
+			<td>".date("d.m.Y H:i", $row['date'])."</td>
+			<td>".$row['sum']." ".$moneycurr."</td>
+			<td><b>".$row['purse']."</b></td>
+			<td>".$row['paysys']."</td>
+			<td class=\"status text-center\">";
 
-		if($row['status'] == 0) {
-			print '<span class="tool"><i class="fa fa-hourglass text-warning "><span class="tip">'.$lang['msg_07'].'</span></span>';
-		} elseif($row['status'] == 1) {
-			print '<span class="tool"><i class="fa fa-clock-o text-blue"></i><span class="tip">'.$lang['msg_03'].'</span></span>';
-		} elseif($row['status'] == 2) {
-			print '<span class="tool"><i class="fa fa-check text-success"></i><span class="tip">'.$lang['msg_04'].'</span></span>';
-		} else {
-			print '<span class="tool"><i class="fa fa-ban text-error"></i><span class="tip">'.$lang['msg_08'].'</span></span>';
-		}
+				if($row['status'] == 0) {
+					print '<span class="tool"><i class="fa fa-hourglass text-warning "><span class="tip">'.$lang['msg_07'].'</span></span>';
+				} elseif($row['status'] == 1) {
+					print '<span class="tool"><i class="fa fa-clock-o text-blue"></i><span class="tip">'.$lang['msg_03'].'</span></span>';
+				} elseif($row['status'] == 2) {
+					print '<span class="tool"><i class="fa fa-check text-success"></i><span class="tip">'.$lang['msg_04'].'</span></span>';
+				} else {
+					print '<span class="tool"><i class="fa fa-ban text-error"></i><span class="tip">'.$lang['msg_08'].'</span></span>';
+				}
 
-		print "</td>
+			print "</td>
 
 		</tr>";
 
@@ -76,9 +91,9 @@ defined('ACCESS') or die();
 			$s = $s + $row['sum'];
 		}
 
-		print "
-		<tr><td class=\"tblbord\"></td><td align=\"right\" class=\"tblbord\"><b>".$lang['total'].":</b></td><td align=\"center\" class=\"tblbord\"><b>".$s."</b> ".$moneycurr."</td><td class=\"tblbord\"></td><td class=\"tblbord\"></td><td class=\"tblbord\"></td></tr></table>";
-
+	print "</table>
+	<p><b>".$lang['total']." внесено: <b>".$s."</b> ".$moneycurr."</b></p>";
+	
 	}
 
 	if ($p) {
@@ -92,3 +107,4 @@ defined('ACCESS') or die();
 	print "<div class=\"pages\"><b>".$lang['pages'].":  </b>".$pervp.$p2left.$p1left." <b>".$p."</b> ".$p1right.$p2right.$nextp."</div>";
 
 ?>
+</div>
